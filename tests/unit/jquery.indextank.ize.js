289d5c6b793a730ac19f95471732cfbaeb72c530
Diego Buthay <dbuthay@linkedin.com>
2012-01-31 16:47:56 -0800

    URL validation allows multiple providers. Closes #14

--- tests/unit/jquery.indextank.ize.js
+++ tests/unit/jquery.indextank.ize.js
@@ -1,6 +1,6 @@
 module("Module Ize", { 
     setup: function() {
-        this.apiurl = "http://some.api.indextank.com";
+        this.apiurl = "http://some.api.provider.tld";
         this.indexName = "someIndexName";
     },
     teardown: function() {
@@ -22,11 +22,25 @@ test( "check data is stored", function() {
 
 
 test( "check apiurl is a url", function() {
-
+  // need a reference to this from inside the closure .. 
+  var self = this;
+  
   raises( function() { 
-      $("#myform").indextank_Ize("not_a_url", this.indexName);
+      $("#myform").indextank_Ize("not_a_url", self.indexName);
   });
 
+  ize = $("#myform").data("Indextank.Ize");
+  equals (ize, undefined);
+});
+
+test( "does not allow private URL", function() {
+  // need a reference to this from inside the closure .. 
+  var self = this;
+
+  raises( function() {
+      $("#myform").indextank_Ize("http://secret@some.api.provider.io", self.indexName);
+  });
+  
   ize = $("#myform").data("Indextank.Ize");
   equals (ize, undefined);
 });
